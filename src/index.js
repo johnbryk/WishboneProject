2de9ffadcad8fe259011b7a3153d630b26a7e802
johnbryk <43798730+johnbryk@users.noreply.github.com>
2023-03-02 09:36:35 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import GoogleLogin from './GoogleLogin.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <GoogleOAuthProvider clientId="741017505698-4h94mgscj6acm22q74p8tvu2kr62ln9b.apps.googleusercontent.com">
+        <React.StrictMode>
+            <GoogleLogin />
+        </React.StrictMode>
+    </GoogleOAuthProvider>)
 ;
 
 //741017505698-4h94mgscj6acm22q74p8tvu2kr62ln9b.apps.googleusercontent.com
